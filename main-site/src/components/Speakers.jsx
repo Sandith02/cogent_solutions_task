8633387ba61a9adfd989ec208d0422d025762235
Sandith02 <lhthenuwara@gmail.com>
2025-07-30 22:49:55 +0530

    Bio updates

--- main-site/src/components/Speakers.jsx
+++ main-site/src/components/Speakers.jsx
@@ -43,6 +43,7 @@ const Speakers = () => {
       title: "Executive Vice President",
       company: "Universal Banking Finastra",
       image: "/Ms. Siobhan Byron.jpg",
+      bio:"Siobhan oversees Universal Banking, including market leading core banking and digital solutions globally. She has more than 25 years of experience in IT and channel management, as well as leading and growing prominent technology companies across financial services, insurance, manufacturing, and public sectors. Siobhan is a strong advocate for diversity and inclusion. She is the Executive Sponsor for the company’s LGBTQ+ and Friends initiatives and supports Women@Finastra. Based in Canada, Siobhan enjoys spending down time with her family and friends, travelling, reading, and watching her son play hockey. "
     },
     {
       id: 3,
@@ -50,13 +51,15 @@ const Speakers = () => {
       title: "Managing Director - Middle East, Africa & Asia-Pacific",
       company: "Finastra Universal Banking",
       image: "/Mr. Rudy Kawmi.jpg",
+      bio: " Rudy Kawmi oversees the Retail Banking business across the Middle East, Africa, and Asia-Pacific regions at Finastra. With a career dedicated to financial technology, Rudy has collaborated with banks on numerous digital transformation initiatives. As a proponent of innovative business models that promote financial inclusion, he is passionate about establishing fintech partnerships across these regions to drive innovation and expand access to next-generation banking systems. "
     },
     {
       id: 4,
       name: "Mr. Narendra Mistry",
       title: "Chief Product and Technology Officer",
       company: "Universal Banking Finastra",
-      image: "/Mr. Narendra Mistry.jpg"
+      image: "/Mr. Narendra Mistry.jpg",
+      bio: " Narendra leads the global product and technology organization for Universal Banking as the Chief Product and Technology Officer (CPTO). Narendra has been in the financial software industry for more than 25 years and his career with Finastra spans more than 13 years in various leadership roles, including running product for International Retail. He has experience building new SaaS businesses in Digital Essence cloud banking, whilst ensuring a robust product and go-to-market strategy. Narendra was instrumental in the very successful Equation N-2 program and the Midas Service Pack program that have made the Midas and Equation business extremely stable over the years. During his time at Finastra, Narendra has built a strong reputation of being a trusted leader who has been central to many successes on our journey. Narendra is based in London. "
     },
     {
       id: 5,
@@ -77,14 +80,16 @@ const Speakers = () => {
       name: "Mr. Karim El Mourabet",
       title: "Solution Consulting Director - MEA",
       company: "Finastra",
-      image: "/Mr. Karim El Mourabet.jpg"
+      image: "/Mr. Karim El Mourabet.jpg",
+      bio: "Karim El Mourabet leads the Retail Banking Solution Consulting team at Finastra, covering Middle East & Africa. He has over 12 years’ experience in retail banking software implementation and consulting and working with Digital Banks. He previously led the Innovation services for Finastra Global Services team and worked on Next Gen implementation services, including core banking migrations. Prior to re-joining Finastra, he was global head of solution consulting at Fidor, working on designing and launching digital banks globally. He holds a Masters of Science in Project Management and a Bachelor’s in Business Administration from the American University of Beirut. "
     },
     {
       id: 8,
       name: "Mr. Ahmed Hamdy Bahey El Din",
       title: "Head of Information Technology",
       company: "Incolease",
-      image: "/Mr. Ahmed Hamdy Bahey El Din.jpeg"
+      image: "/Mr. Ahmed Hamdy Bahey El Din.jpeg",
+      bio:" Ahmed Hamdy is a results-driven IT professional with extensive experience leading IT departments and delivering innovative solutions. As the IT Head at Incolease, he manages all IT operations, ensuring strategic alignment, process optimization, and efficient resource management. With over 18 years of experience, his career highlights include managing complex projects at EFG-HERMES, Corplease, and Atos, focusing on business process automation and efficiency improvements. Ahmed's expertise includes ERP systems, leasing, factoring, and securitization, and he holds certifications in CBAP® and ITIL® Foundation. "
     },
     {
       id: 9,
@@ -105,21 +110,24 @@ const Speakers = () => {
       name: "Mr. Hamid Nirouzad",
       title: "Managing Director - Africa",
       company: "Finastra Universal",
-      image: "/Mr. Hamid Nirouzad.jpg"
+      image: "/Mr. Hamid Nirouzad.jpg",
+      bio: "Hamid Nirouzad is a seasoned professional in the fintech industry. He began his career at National Westminster Bank in the UK before joining Midas-Kapiti in 1994. Throughout his tenure at Finastra, Hamid has played a pivotal role in various business aspects across Europe and the MEA regions. With a robust background in both retail and corporate banking, he possesses a comprehensive understanding of the banking sector. "
     },
     {
       id: 12,
       name: "Mr. Matthew Hughes",
       title: "Head of FS&I, International Markets",
       company: "Atos",
-      image: "/Mr. Matthew Hughes.jpg"
+      image: "/Mr. Matthew Hughes.jpg",
+      bio: " Matthew Hughes is a seasoned professional with over 25 years of experience in the financial services and insurance sectors. Matthew worked for 17 years at Royal Bank of Scotland in various leadership roles, including Country Head of Operations in China, Head of Retail and Commercial Banking in China, Head of Core Banking Operations for England and Wales, and Head of Consumer Finance for Spain and Portugal. At Atos, he serves as Global Client Executive Partner and Head of the Financial Services & Insurance Sector for International Markets (covering Middle East, APAC, Latin America, Switzerland, Iberia and South East Europe). In this role, Matthew leads strategic client engagement and initiatives around Platform and/or Mainframe Modernisation and Migrations, Cloud, Cybersecurity and the full range of end-to-end managed services that Atos offers, as well as the management of key client relationships, leveraging his extensive industry expertise to drive business growth and innovation. In 2022, Atos in Asia Pacific announced Matthew's appointment as the Global Client Executive Partner for our largest customer in APAC (Global Bank), underscoring his pivotal role in strengthening the partnership between Atos and the bank. Matthew is based in Singapore and is bilingual in Spanish."
     },
     {
       id: 13,
       name: "Mr. Daragh O'Byrne",
       title: "Senior Director, Marketing",
       company: "Universal Banking, Finastra",
-      image: "/Mr. Daragh O'Byrne.JPG"
+      image: "/Mr. Daragh O'Byrne.JPG",
+      bio:"Daragh leads the Finastra Universal Banking marketing team. For more than 25 years he has worked in the Financial Services application software market, focusing on ensuring that software provides tangible business benefits for customers, and that those business benefits are explained in a clear, concise and compelling way via an integrated set of creative marketing activities that generate results. He combines his background in technology with a decade helping banks all over the world translate their vision into implemented reality to position sophisticated banking platforms in terms of the business problems they solve and the opportunities they enable. Daragh focuses on turning strategy into action and turning action into results in a co-ordinated way. A passionate believer in technology's power to transform and enrich, he believes in explaining its benefits in easy to an understand, educational and entertaining way. "
     },
     {
       id: 14,
@@ -127,6 +135,7 @@ const Speakers = () => {
       title: "Co-Founder and CEO",
       company: "CARITech",
       image: "/Dr. Ismail Ali.jpg",
+      bio:" Dr. Ismail Ali is the Co-Founder and CEO of CARITech, a digital banking solutions provider supporting financial institutions across the Middle East and Africa. With over 25 years of experience in financial technology, Dr. Ali has led core banking modernization programs for both Islamic and conventional banks—helping them overcome legacy challenges and respond to dynamic market demands. He has held senior positions with leading global technology firms and now leads CARITech’s mission to eliminate technology barriers through structured, standards-driven delivery. CARITech operates in the Americas and is expanding rapidly across MEA, delivering high-quality transformation services using CMMI Level 3-compliant methodologies. Dr. Ali holds a Doctorate in Business Management, an MBA, and is an alumnus of Harvard Business School. He regularly collaborates with banks, regulators, and fintech leaders to enable inclusive, future-ready financial ecosystems."
     },
     {
       id: 15,
